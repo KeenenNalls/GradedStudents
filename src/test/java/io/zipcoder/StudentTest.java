6af42d7958f8331666fe5fa6556cf25e658b6717
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-08-14 13:47:10 -0400

    test finished

--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,66 @@
 package io.zipcoder;
+import org.junit.Test;
 
 public class StudentTest {
+    @Test
+    public void getExamScoresTest() {
+        // : Given
+        String firstName = "Keenen";
+        String lastName = "Nalls";
+        Double[] examScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void addExamScoreTest() {
+        // : Given
+        String firstName = "Keenen";
+        String lastName = "Nalls";
+        Double[] examScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void setExamScoreTest() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        student.setExamScore(1, 150.0);
+        String output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void getAverageExamScoreTest() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        Double output = student.getAverageExamScore();
+
+        // Then
+        System.out.println(output);
+    }
 
 }
